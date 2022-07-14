41218c6a045f774724eb1c35a2ba77a2134ed75a
nicholas <nja8161995@gmail.com>
2022-07-14 15:54:39 -0500

    Completed: repeatString Challenge Complete

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    let output = ``;
 
+    if (num >= 0) {
+        for (let i = 0; i < num; i++) {
+            output += str;
+        }
+    } 
+    else {
+        output = `ERROR`
+    }
+
+    return output;
 };
 
 // Do not edit below this line
