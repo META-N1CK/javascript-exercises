0e8eb0b396ba72940f8f41ad31f51779e0fc42ef
nicholas <nja8161995@gmail.com>
2022-07-15 09:56:36 -0500

    Completed: sumAll Challenge Completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,28 @@
-const sumAll = function() {
+const sumAll = function(inputNum, loopNum) {
+    let total = 0;
 
+    if (typeof(inputNum) !== `number` || typeof(loopNum) !== `number`) {
+        return `ERROR`;
+    }
+    else {
+        if (inputNum <= 100 && inputNum > 0) {
+            for (let i = 0; i < loopNum; i++) {
+                total += inputNum;
+                inputNum++;
+            }
+        }
+        else if (inputNum >= 100) {
+            for (let i = inputNum; i >= loopNum; i--) {
+                total += inputNum;
+                inputNum--;
+            }
+        }
+        else {
+            return `ERROR`;
+        }
+    }
+
+    return total;
 };
 
 // Do not edit below this line
