4e22d54bd52b9d8a6e182f193a9c6e423e8a8601
nicholas <nja8161995@gmail.com>
2022-07-15 08:27:36 -0500

    Completed: removeFromArray Challenge Completed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, input1, input2, input3, input4) {
+   
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === input1 || arr[i] === input2 || arr[i] === input3 || arr[i] === input4) {
+            arr.splice(i, 1);
+            i--;
+        } 
+    }
 
+    return arr;
 };
 
 // Do not edit below this line
