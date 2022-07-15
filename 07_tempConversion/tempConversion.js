65655482ff766047bb4caeb357cdc28f99b66a92
nicholas <nja8161995@gmail.com>
2022-07-15 15:30:34 -0500

    Comleted: tempConversion Challenge Completed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,18 @@
-const ftoc = function() {
+const ftoc = function(temp) {
+  
+  // Subtract 32 and divide by 1.8
+  let ftocMath = (temp - 32) / 1.8;
+
+  return Math.round(ftocMath * 10) / 10;
 
 };
 
-const ctof = function() {
+const ctof = function(temp) {
+
+  // Multiply by 1.8 then add 32
+  let ctofMath = (temp * 1.8) + 32;
+
+  return Math.round(ctofMath * 10) / 10;
 
 };
 
