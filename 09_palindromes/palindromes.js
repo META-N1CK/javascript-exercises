59cae6454507292482fecacfe59110a2b6048581
nicholas <nja8161995@gmail.com>
2022-07-22 10:57:44 -0500

    Completed: palindromes Challenge Completed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,23 @@
-const palindromes = function () {
+const palindromes = function (str) {
 
+    let fwrdStr = str.replace(/[^A-Za-z]/gi, '');
+    // let fwrdStr = "";
+    let bwrdStr = "";
+
+    // for (let i = 0; i < palnStr.length; i++) {
+    //     fwrdStr += palnStr[i];
+    // }
+
+    for (let i = fwrdStr.length - 1; i >= 0; i--) {
+        bwrdStr += fwrdStr[i];
+    }
+        
+    if (fwrdStr.toLowerCase() === bwrdStr.toLowerCase()) {
+        return true;
+    }
+    else {
+        return false;
+    }
 };
-
 // Do not edit below this line
 module.exports = palindromes;
