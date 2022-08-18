b4a47c490a397eb04d48e136cab26d32fe0400c8
nicholas <nja8161995@gmail.com>
2022-08-18 13:28:30 -0500

    Completed Fibonacci Challenge

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+  //Get number
+  const fibArray = [0, 1];
+  //Loop number
+  if (num > 0) {
+    for (let i = 2; i <= num; i++) {
+      //Store loop of number in an array
+      fibArray[i] = fibArray[i - 1] + fibArray[i - 2];
+    }
+    return fibArray[num];
+  } else {
+    return "OOPS";
+  }
 };
 
 // Do not edit below this line
