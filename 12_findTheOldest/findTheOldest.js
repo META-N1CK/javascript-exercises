de3e7dd3fd0293024e1c180e026fbd75f4ba880e
nicholas <nja8161995@gmail.com>
2022-08-18 15:08:20 -0500

    Completed finTheOldest Challenge

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,12 @@
-const findTheOldest = function() {
-
-};
+const findTheOldest = (people) =>
+  people.reduce((oldest, current) => {
+    if (!oldest.yearOfDeath) {
+        oldest.yearOfDeath = new Date().getFullYear();
+      }
+    const oldestAge = oldest.yearOfDeath - oldest.yearOfBirth;
+    const currentAge = current.yearOfDeath - current.yearOfBirth;
+    return currentAge > oldestAge ? current : oldest;
+  });
 
 // Do not edit below this line
 module.exports = findTheOldest;
